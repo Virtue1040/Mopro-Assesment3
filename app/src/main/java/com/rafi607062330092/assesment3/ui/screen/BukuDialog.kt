0be0bd95b38939dce835288cc17fda3716ba6bb2
Virtue1040 <rafi.hidayat1313@gmail.com>
2025-06-12 03:22:10 +0700

    Memindahkan image decoder bitmap dari MainScreen ke BukuDialog

--- app/src/main/java/com/rafi607062330092/assesment3/ui/screen/BukuDialog.kt
+++ app/src/main/java/com/rafi607062330092/assesment3/ui/screen/BukuDialog.kt
@@ -1,7 +1,13 @@
 package com.rafi607062330092.assesment3.ui.screen
 
+import android.content.ContentResolver
 import android.content.res.Configuration
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -23,24 +29,48 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
+import com.canhub.cropper.CropImageView
 import com.rafi607062330092.assesment3.R
+import com.rafi607062330092.assesment3.model.Buku
+import com.rafi607062330092.assesment3.network.BukuApi
 import com.rafi607062330092.assesment3.ui.theme.Mobpro1Theme
 
 @Composable
 fun BukuDialog(
-    bitmap: Bitmap?,
+    buku: Buku? = null,
     onDismissRequest: () -> Unit,
-    onConfirmation: (String, String, String) -> Unit
+    onConfirmation: (String, String, String, Bitmap?) -> Unit
 ) {
+    val context = LocalContext.current
+
     var judul by remember { mutableStateOf("") }
     var penulis by remember { mutableStateOf("") }
     var penerbit by remember { mutableStateOf("") }
+
+    var bitmap: Bitmap? by remember { mutableStateOf(null) }
+    val launcher = rememberLauncherForActivityResult(CropImageContract()) {
+        bitmap = getCroppedImage(context.contentResolver, it)
+    }
+
+    if (buku != null) {
+        judul = buku.judul
+        penulis = buku.penulis
+        penerbit = buku.penerbit
+    }
 
     Dialog(
         onDismissRequest = {
@@ -55,11 +85,46 @@ fun BukuDialog(
                 modifier = Modifier.padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Image(
-                    bitmap = bitmap!!.asImageBitmap(),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth().aspectRatio(1f)
-                )
+                if (buku != null && bitmap == null) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(
+                                BukuApi.getImageUrl(buku.id_buku)
+                            )
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = stringResource(R.string.gambar, buku.judul),
+                        contentScale = ContentScale.Crop,
+                        placeholder = painterResource(id = R.drawable.loading_img),
+                        error = painterResource(id = R.drawable.broken_img),
+                        modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                    )
+                } else {
+                    if (bitmap != null) {
+                        Image(
+                            bitmap = bitmap!!.asImageBitmap(),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                        )
+                    }
+                }
+                OutlinedButton(
+                    onClick = {
+                        val options = CropImageContractOptions(
+                            null, CropImageOptions(
+                                imageSourceIncludeGallery = false,
+                                imageSourceIncludeCamera = true,
+                                fixAspectRatio = true
+                            )
+                        )
+                        launcher.launch(options)
+                    },
+                    modifier = Modifier.padding(top = 8.dp).fillMaxWidth()
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.pilih_gambar)
+                    )
+                }
                 OutlinedTextField(
                     value = judul,
                     onValueChange = {
@@ -67,7 +132,7 @@ fun BukuDialog(
                     },
                     label = {
                         Text(
-                            text = stringResource(id = R.string.nama)
+                            text = stringResource(id = R.string.judul)
                         )
                     },
                     maxLines = 1,
@@ -84,7 +149,7 @@ fun BukuDialog(
                     },
                     label = {
                         Text(
-                            text = stringResource(id = R.string.nama_latin)
+                            text = stringResource(id = R.string.penulis)
                         )
                     },
                     maxLines = 1,
@@ -101,7 +166,7 @@ fun BukuDialog(
                     },
                     label = {
                         Text(
-                            text = stringResource(id = R.string.nama_latin)
+                            text = stringResource(id = R.string.penerbit)
                         )
                     },
                     maxLines = 1,
@@ -127,9 +192,9 @@ fun BukuDialog(
                     }
                     OutlinedButton(
                         onClick = {
-                            onConfirmation(judul, penulis, penerbit)
+                            onConfirmation(judul, penulis, penerbit, bitmap)
                         },
-                        enabled = judul.isNotEmpty() && penulis.isNotEmpty() && penerbit.isNotEmpty(),
+                        enabled = judul.isNotEmpty() && penulis.isNotEmpty() && penerbit.isNotEmpty() && (buku != null || bitmap != null),
                         modifier = Modifier.padding(8.dp)
                     ) {
                         Text(
@@ -142,15 +207,36 @@ fun BukuDialog(
     }
 }
 
+
+@Suppress("deprecation")
+private fun getCroppedImage(
+    resolver: ContentResolver,
+    result: CropImageView.CropResult
+): Bitmap? {
+    if (!result.isSuccessful) {
+        Log.e("IMAGE", "Error: ${result.error}")
+        return null
+    }
+
+    val uri = result.uriContent ?: return null
+
+    return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+        MediaStore.Images.Media.getBitmap(resolver, uri)
+    } else {
+        val source = ImageDecoder.createSource(resolver, uri)
+        ImageDecoder.decodeBitmap(source)
+    }
+}
+
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun AddDialogPreview() {
     Mobpro1Theme {
         BukuDialog (
-            bitmap = null,
+            buku = null,
             onDismissRequest = {},
-            onConfirmation = { _,_,_ ->}
+            onConfirmation = { _,_,_,_ ->}
         )
     }
 }
