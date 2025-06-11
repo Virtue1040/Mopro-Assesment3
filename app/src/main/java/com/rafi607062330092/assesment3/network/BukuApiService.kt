8e7e0de060b2f396920fe3a4dee15ec9bdf5d1b3
Virtue1040 <rafi.hidayat1313@gmail.com>
2025-06-12 01:24:32 +0700

    menambahkan debug URL

--- app/src/main/java/com/rafi607062330092/assesment3/network/BukuApiService.kt
+++ app/src/main/java/com/rafi607062330092/assesment3/network/BukuApiService.kt
@@ -1,14 +1,19 @@
 package com.rafi607062330092.assesment3.network
 
 import com.rafi607062330092.assesment3.model.Buku
+import com.rafi607062330092.assesment3.model.BukuStatus
 import com.rafi607062330092.assesment3.model.OpStatus
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
 import okhttp3.RequestBody
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.DELETE
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
@@ -16,7 +21,15 @@ import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Query
 
-private const val BASE_URL = "https://gh.d3ifcool.org/"
+private const val BASE_URL = "https://rattler-intent-cricket.ngrok-free.app/api/"
+
+private val logging = HttpLoggingInterceptor().apply {
+    level = HttpLoggingInterceptor.Level.BODY
+}
+
+private val client = OkHttpClient.Builder()
+    .addInterceptor(logging)
+    .build()
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -25,34 +38,35 @@ private val moshi = Moshi.Builder()
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
+    .client(client)
     .build()
 
 interface BukuApiService {
     @GET("buku")
-    suspend fun getBuku(
-        @Header("Authorization") token: String
-    ): List<Buku>
+    suspend fun getBuku(): BukuStatus
 
     @Multipart
     @POST("buku")
     suspend fun postBuku(
         @Header("Authorization") token: String,
-        @Part("nama") nama: RequestBody,
-        @Part("namaLatin") namaLatin: RequestBody,
+        @Part("judul") judul: RequestBody,
+        @Part("penulis") penulis: RequestBody,
+        @Part("penerbit") penerbit: RequestBody,
         @Part image: MultipartBody.Part
     ): OpStatus
 
-    @DELETE("buku")
+    @DELETE("buku/{id_buku}")
     suspend fun deleteBuku(
         @Header("Authorization") token: String,
-        @Query("id") id: String,
+        @Query("id_buku") id_buku: Long
     ): OpStatus
 
+    @FormUrlEncoded
     @POST("register")
     suspend fun postRegister(
-        @Part("nama") nama: RequestBody,
-        @Part("email") namaLatin: RequestBody,
-        @Part("password") idToken: RequestBody,
+        @Field("name") nama: String,
+        @Field("email") email: String,
+        @Field("password") password: String
     ): OpStatus
 }
 
